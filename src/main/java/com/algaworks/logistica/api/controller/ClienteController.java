9b3d46778b980a59c3e3f91c4e283048c8ccdd0d
felipeperaltacheung <f33___@hotmail.com>
2022-05-17 00:45:50 -0300

    Projeto iniciado, e adicionando GetMapping clientes, retornando uma lista de clientes

---
+++ src/main/java/com/algaworks/logistica/api/controller/ClienteController.java
@@ -0,0 +1,32 @@
+package com.algaworks.logistica.api.controller;
+
+import domain.model.Cliente;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class ClienteController {
+
+    @GetMapping("/clientes")
+    public List<Cliente> listar(){
+        var cliente1 = new Cliente();
+        cliente1.setId(1L);
+        cliente1.setNome("je");
+        cliente1.setEmail("jacare@jacare");
+        cliente1.setTelefone("1010010101001");
+
+        var cliente2 = new Cliente();
+        cliente2.setId(2L);
+        cliente2.setNome("ke");
+        cliente2.setEmail("jake@jake");
+        cliente2.setTelefone("202020000202020");
+
+        return Arrays.asList(cliente1, cliente2);
+
+    }
+
+}
---
+++ src/test/java/com/algaworks/logistica/logistica/LogisitcaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.logistica.logistica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisitcaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
